836857fd77f1f3c7cf0c2f7dcdb6a3254bf75be0
Vitor <vitor.sofiati@live.com>
2025-01-09 21:37:14 -0300

    Criados componentes de eventos e palestrantes, assim como o fetch na api criada no back-end

---
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-eventos',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.scss',
+})
+export class EventosComponent {
+  eventos: any;
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  constructor(private http: HttpClient) {}
+
+  public getEventos(): void {
+    this.http.get('https://localhost:7189/api/eventos').subscribe({
+      next: (response) => (this.eventos = response),
+      error: (error) => console.log(error),
+    });
+  }
+}
